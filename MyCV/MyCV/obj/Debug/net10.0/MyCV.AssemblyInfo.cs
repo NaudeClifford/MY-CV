86b8f2e3d21d4e60a055696312dbe17a3e877aba
Naude-Clifford <cliffordnaude990@gmail.com>
2026-05-06 12:58:25 +0200

    updated information
    added description, strengths, and profile picture.

--- MyCV/MyCV/obj/Debug/net10.0/MyCV.AssemblyInfo.cs
+++ MyCV/MyCV/obj/Debug/net10.0/MyCV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b522452fc7394d01f8fc8c6e9f274818dc4e8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70211db04ddfd3632a29f9c90a800fa2bf24a428")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
